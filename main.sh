9da85aedb39c50470dc1a3e4862d8fb2b9c0a53e
danzee1 <cab00676@gmail.com>
2023-10-07 10:50:05 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+# Check if the 'prips' command is available, and if not, install it
+if ! command -v prips &> /dev/null; then
+    read -p "'prips' command not found. Do you want to install it? (y/n): " install_prips
+    if [ "$install_prips" = "y" ]; then
+        sudo apt-get update
+        sudo apt-get install -y prips
+    else
+        echo "You chose not to install 'prips'. Exiting."
+        exit 1
+    fi
+fi
+
 # Prompt the user for an ASN number
 read -p "Enter an ASN number: " asn
 
 # Prompt the user for an output file name
 read -p "Enter an output file name (or press Enter to use 'ips.out'): " output_file
 output_file="${output_file:-ips.out}"
-
-# Check if the 'prips' command is available
-if ! command -v prips &> /dev/null; then
-    echo "Error: 'prips' command not found. Please install it with 'apt-get install prips'." >&2
-    exit 1
-fi
 
 # Get IP ranges associated with the ASN and append them to the specified output file
 for range in $(whois -h whois.radb.net -- "-i origin $asn" | grep -Eo "([0-9.]+){4}/[0-9]+"); do
